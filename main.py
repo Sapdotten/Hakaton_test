8ebd7251abe9f54eb96f8391d89258a5c924d4e4
EvaKorsya <korsaeva24@gmail.com>
2023-11-12 15:05:20 +0400

    test commit

--- main.py
+++ main.py
@@ -1,2 +1,25 @@
-print('Sapdotten commit')
 import bs4
+print('hui')
+
+
+def main():
+    print("hi")
+
+
+
+def start():
+    global __commands
+    __commands = {
+        'task': 'задач',
+        'hello': 'прив|здрав',
+        'about_club': 'что такое robotic?',
+        'projects': 'проекты и мероприятия',
+        'join_club': 'как попасть в robotic?',
+        'remind': 'напомни'
+        'about_club': 'что такое robotic?',
+        'projects': 'проекты и мероприятия',
+        'join_club': 'как попасть в robotic?',
+        'remind': 'напомни'
+    }
+def p():
+    pass
