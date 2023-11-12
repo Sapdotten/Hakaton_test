4c0d8262e6abca6edb6072875346f01481035522
moter1026 <matvej.sidoroff2016@yandex.ru>
2023-11-12 19:49:42 +0400

    работает подключение к back4app

--- main.py
+++ main.py
@@ -1,23 +1,17 @@
-import bs4
-print('hui')
+import http.client
+import json
+
+connection = http.client.HTTPSConnection('parseapi.back4app.com', 443)
+connection.connect()
+connection.request('GET', '/schemas/_Session', '', {
+    "X-Parse-Application-Id": "bjoIcJFXfuwVRVDvjFOW5DXG931cVWj7TvezpzcU",
+    "X-Parse-Master-Key": "kVSXIzLhcx3rRXKwj4aLi18okIxLiL3P8egROR9u"
+})
+
+# "X-Parse-REST-API-Key": "evZga6ZDf4sIoydxICnJ1DZPOcgs5BQyCQYlDKXg"
+# "X-Parse-Master-Key": "kVSXIzLhcx3rRXKwj4aLi18okIxLiL3P8egROR9u"
+result = json.loads(connection.getresponse().read())
+print(result)
 
 
-def main():
-    print("hi")
 
-
-
-def start():
-    global __commands
-    __commands = {
-        'task': 'задач',
-        'hello': 'прив|здрав',
-        'about_club': 'что такое robotic?',
-        'projects': 'проекты и мероприятия',
-        'join_club': 'как попасть в robotic?',
-        'remind': 'напомни',
-        'about_club': 'что такое robotic?',
-        'projects': 'проекты и мероприятия',
-        'join_club': 'как попасть в robotic?',
-        'remind': 'напомни'
-    }
