3ebb87f34cdb22d2c44ee9b35f73779037329cfb
Sapdotten <semenova.a.p.04@gmail.com>
2023-11-13 17:14:17 +0400

    попытка написать регистрацию бота в сессии

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
 """Файл, в котором будет происходить запуск бота и его найстрока
 (считывание переменных окружения, импорт наших моудлей и т.п.)"""
 import os
+import rest_api
 
 # SESSION_ID – id сессии, в которой должен участвовать бот
 # BOT_URL – url для обратной коммуникации медиатора с ботом. Должен передаваться в запросе на регистрацию в сессии
 # MEDIATOR_URL – base url, по которому бот может зарегистрироваться в сессии
 
+
 if __name__ == "__main_":
+    bot_id = ""
+    password = ""
     # id сессии, в которой должен участвовать бот
     SESSION_ID = os.environ.get('SESSION_ID')
     # URL для обратной коммуникации медиатора с ботом. Должен передаваться в запросе на регистрацию в сессии
     BOT_URL = os.environ.get('BOT_URL')
     # base url, по которому бот может зарегистрироваться в сессии
     MEDIATOR_URL = os.environ.get('MEDIATOR_URL')
+
+    rest_api.register_bot(mediator_url=MEDIATOR_URL,
+                          session_id=SESSION_ID,
+                          bot_url=BOT_URL,
+                          bot_id=bot_id,
+                          password=password)
--- rest_api.py
+++ rest_api.py
@@ -1,2 +1,24 @@
 """Файл для реализцаии обмена запросами с сервером медитором
 (возможно поделим на два файла - для получения запрсов и для отправки)"""
+import requests
+import json
+
+
+def register_bot(mediator_url: str, session_id: str, bot_url: str, bot_id: str, password: str):
+    url = f"http://{mediator_url}/sessions/{session_id}/registration"
+    headers = {'Content-Type': 'application/json'}
+    bot_data = {
+        "bot_id": bot_id,
+        "password": password,
+        "bot_url": bot_url
+    }
+    response = requests.post(url, data=json.dumps(bot_data), headers=headers)
+    if response.status_code == 200:
+        print("Бот успешно зарегистрирован в сессии")
+        return response.json()
+    elif response.status_code == 400:
+        print("Некорректный запрос")
+    elif response.status_code == 404:
+        print("Не найдена игровая сессия с таким session_id")
+    else:
+        print(f"Необработанный статус кода: {response.status_code}")
