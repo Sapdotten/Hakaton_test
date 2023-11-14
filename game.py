1f51aba590e7caa1c8fb112e0ba6959057248366
Sapdotten <semenova.a.p.04@gmail.com>
2023-11-15 02:59:45 +0400

    Средиопасных уомбинацией теперь находит перекрестки

--- game.py
+++ game.py
@@ -1,6 +1,7 @@
 """Файл для реализации логики игры"""
 global char
 import time
+from typing import Union
 
 
 def set_figure(char_: str):
@@ -63,7 +64,7 @@ class Field:
                 ind = self.field[i * 19:i * 19 + 19].find(pattern)
                 if ind != -1:
                     return i * 19 + ind, pattern, 'row'
-                indw = self.field[i:i + 19:19].find(pattern)
+                indw = self.field[i:i + 19 * 18:19].find(pattern)
                 if indw != -1:
                     return indw * 19 + i, pattern, 'column'
         for i in range(0, 19):
@@ -81,20 +82,61 @@ class Field:
                 if ind_ld != -1:
                     return (ind_ld + i) * 19 + ind_ld, pattern, 'diagonal_down'
 
+    def check_neighbors(self, id_: int, type_: str) -> bool:
+        """
+        Проверяет наличие соседей для выявления Т образного узла
+        :param id_: номер ячейки, соседей которой нужно проверить
+        :param type_: перпендикулярно оси проверки
+        :return: true или false
+        """
+        if type_ == 'row':
+            #         проверяем сверху и снизу
+            if self.field[id_ - 19] == self.enemy_char and self.field[id_ + 19] == self.enemy_char:
+                return True
+            return False
+        else:
+            if self.field[id_ - 1] == self.enemy_char and self.field[id_ + 1] == self.enemy_char:
+                return True
+            return False
+
+    def is_T_pattern(self, id_: int, pattern: str, type_: str) -> Union[int, None]:
+        pattern = pattern.replace('x', 'p').replace('o', 'p')
+        i = 1
+        if type_ == 'diagonal_up' or type_ == 'diagonal_down':
+            return None
+        elif type_ == 'column':
+            i = 19
+        if (pattern == 'ppp_p' or pattern == '_pp_p_') and self.check_neighbors(id_ + 3*i, type_):
+            return id_ + 3 * i
+        elif (pattern == 'pppp_' or pattern == '_p_pp_') and self.check_neighbors(id_ + 5*i, type_):
+            return id_ + 5*i
+        elif (pattern == 'pp_pp' or pattern == '_p_pp_') and self.check_neighbors(id_ + 2*i, type_):
+            return id_ + 2*i
+        elif pattern == 'p_ppp' and self.check_neighbors(id_ + 1*i, type_):
+            return id_ + 1*i
+        elif (
+                pattern == '_pppp' or pattern == '_pp_p_' or pattern == '_ppp__' or pattern == '_p_pp_') and self.check_neighbors(
+            id_, type_):
+            return id_
+        elif (pattern == '_pp_p_' or pattern == '_ppp__') and self.check_neighbors(id_ + 4*i, type_):
+            return id_ + 4*i
+
 
 char = 'o'
 c = "_oo___oo_xoxo__xoo_o_oxx_oo_x______o_______x___oo_x_x_o_x_______oxo___o_x__o_o______x__o_____x__o_____o______x_____xoxoo___xo_____o__x_x__________x__x____o_xo__x__o___x_______o_x______xo______oxo_x_xx__xox___ox____x_oo__ox_x_x___o__________x______________o_____x____o___x___xo___x__x_xo__x_x___ox___x_______x____x_o_x__x_o__ox__o__x__ox_x_____x_oo_____x____o_ox"
 
 a = "_____________x_____"
-a += "______________x____"
-a += "_______________x___"
-a += "___________________"
-a += "_________________x_"
+a += "____________x_x____"
+a += "_____________x_____"
+a += "_____________x_____"
+a += "_____________x_____"
 a += "___________________" * 14
 
 f = Field(c)
 t1 = time.time()
-print(f.is_danger_pattern())
+l = f.is_danger_pattern(f.enemy_char)
+print(l)
+print(f.is_T_pattern(l[0], l[1], l[2]))
 t2 = time.time()
 print(t2 - t1)
 for i in range(0, 19):
