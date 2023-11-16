539f172daec5f867529b93c3b6aab493ed93f860
moter1026 <matvej.sidoroff2016@yandex.ru>
2023-11-16 17:32:57 +0400

    добавил Dockerfile и requirements.txt, где прописаны зависимости, убрал лишнее из main.py

--- main.py
+++ main.py
@@ -1,70 +1,66 @@
 import os
 import json
 import requests
-import http
 from flask import Flask, request, jsonify
-from flask.cli import load_dotenv
 from urllib.parse import urlparse
 
+# в эту переменную потом запишется та фигура, за которую мы будем играть
 figure = 0
 # Инициализация Flask приложения
 app = Flask(__name__)
-
 
 BOT_URL = os.environ["BOT_URL"]
 parsed_url = urlparse(BOT_URL)
 host = parsed_url.hostname
 port = parsed_url.port
-# Получение переменных окружения
-SESSION_ID =  os.environ['SESSION_ID']
-# BOT_URL =  os.environ['BOT_URL']
-MEDIATOR_URL =  os.environ['MEDIATOR_URL']
 
-# load_dotenv()
+SESSION_ID = os.environ['SESSION_ID']
+MEDIATOR_URL = os.environ['MEDIATOR_URL']
+
 BOT_ID = os.environ["BOT_ID"]
 BOT_PASSWORD = os.environ["BOT_PASSWORD_DOCKERFILE"]
 
-@app.route('/', methods=['GET'])
-def mainPage():
-    return "<h1>ГЛАВНАЯ СТРАНИЦА</h1>"
 
 # Маршрут для обработки ходов медиатора
 @app.route('/bot/turn', methods=['POST'])
 def make_move():
-    #Получение данных из запроса
-    
-    print("ДЕЛАЮ ХОД")
-    data = request.get_json()
+    # Получение данных из запроса
+
+    # вывод значений для проверки работоспособности кода
+    # print("ДЕЛАЮ ХОД")
+    make_move_data = request.get_json()
     # Ваш алгоритм для выбора хода
-    print(data["game_field"])
-    
-    move = {"game_field": make_best_move(data["game_field"])}
+    print(make_move_data["game_field"])
+
+    move = {"game_field": make_best_move(make_move_data["game_field"])}
 
     response = move
     return jsonify(response), 200, {'Content-Type': 'application/json'}
+
 
 # Ваш алгоритм для выбора лучшего хода
 def make_best_move(board):
     index = board.find("_")
     # Выполнить замену первого символа "_"
-    new_string = board[:index] + figure + board[index+1:]
-    return new_string  
-# Возвращаем демонстрационный ход
+    new_string = board[:index] + figure + board[index + 1:]
+    return new_string
 
 
 url = f'{MEDIATOR_URL}/sessions/{SESSION_ID}/registration'
-print(url)
+# вывод значений для проверки работоспособности кода
+# print(url)
 headers = {
-'accept': 'application/json',
-'Content-Type': 'application/json'
+    'accept': 'application/json',
+    'Content-Type': 'application/json'
 }
 data1 = {
-"bot_id" : BOT_ID,
-"password" : BOT_PASSWORD,
-"bot_url" : BOT_URL
+    "bot_id": BOT_ID,
+    "password": BOT_PASSWORD,
+    "bot_url": BOT_URL
 }
 data = json.dumps(data1)
-print(data)
+# вывод значений для проверки работоспособности кода
+# print(data)
 res = requests.post(url, headers=headers, data=data)
 
 # Проверка наличия содержимого в ответе
@@ -76,18 +72,15 @@ if res.status_code == 200:
 else:
     print(f"Error: {res.status_code}")
 
-
-
-if (__name__ == "__main__"):
-    print("я зашёл")
-    print(BOT_URL)
-    print(SESSION_ID)
-    print(MEDIATOR_URL)
-    print(BOT_ID)
-    print(BOT_PASSWORD)
-    print(host)
-    print(port)
+if __name__ == "__main__":
+    # вывод значений для проверки работоспособности кода
+    # print("я зашёл")
+    # print(BOT_URL)
+    # print(SESSION_ID)
+    # print(MEDIATOR_URL)
+    # print(BOT_ID)
+    # print(BOT_PASSWORD)
+    # print(host)
+    # print(port)
     # app.run(host=f'0.0.0.0', port=port)
     app.run(host=f'{host}', port=port)
-
-    
